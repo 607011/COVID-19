393b06d2164108232e944baf7a29d20ad2dc0956
Oliver Lau <oliver@ersatzworld.net>
2020-03-28 14:07:59 +0100

    Completely overhauled data evaluation.

---
+++ _bin/update-data.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+import subprocess
+import datetime as dt
+import json
+import math
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit
+
+def parse_timeline(filename, key, selected_country, result):
+  with open(filename, 'r') as confirmed:
+    first_line = confirmed.readline().strip()
+    if not 'dates' in result:
+      result['dates'] = [dt.datetime.strptime(d, '%m/%d/%y') for d in first_line.split(',')[4:]]
+    for line in confirmed:
+      row = line.strip().split(',')
+      if row[1] == selected_country:
+        result[key] = [int(v) for v in row[4:]]
+        return
+
+def parse_latest(filename, selected_country, result):
+  with open(filename, 'r') as latest:
+    latest.readline().strip()  # skip header
+    for line in latest:
+      row = line.strip().split(',')
+      if row[0] == selected_country:
+        result['latest'] = {
+          'last_update': row[1],
+          'where': {
+            'lat': float(row[2]),
+            'lon': float(row[3]),
+          },
+          'total': int(row[4]),
+          'deaths': int(row[5]),
+          'recovered': int(row[6]),
+          'active': int(row[7]),
+        }
+        return
+
+def main():
+  subprocess.call(['git', 'submodule', 'update', '--remote'])
+  selected_country = 'Germany'
+  json_file = 'data/current.json'
+  result = { 'country': selected_country}
+  parse_latest('COVID-19-web-data/data/cases_country.csv', selected_country, result)
+  parse_timeline('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv', 'total', selected_country, result)
+  parse_timeline('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv', 'recovered', selected_country, result)
+  parse_timeline('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', 'deaths', selected_country, result)
+  result['active'] = []
+  for i in range(len(result['total'])):
+    result['active'].append(result['total'][i] - result['recovered'][i] - result['deaths'][i])
+  result['doubling_rates'] = []
+  for i in range(1, len(result['active'])):
+    prev_cases = result['active'][i - 1]
+    curr_cases = result['active'][i]
+    if prev_cases == curr_cases:
+        result['doubling_rates'].append(None)
+    elif prev_cases > 0 and curr_cases > 0:
+        rate = curr_cases / prev_cases
+        doubling_rate = 1 / np.log2(rate)
+        result['doubling_rates'].append(round(doubling_rate, 2))
+    else:
+        result['doubling_rates'].append(None)
+
+  def corona_curve(x, b0, x0, k, s):
+    return s * 1 / (1 + np.exp(-1 * k * s * (x - x0)) * (s / b0 - 1))
+
+  first_day_with_quarantine_effect = dt.datetime(year=2020, month=3, day=20)
+  data = pd.DataFrame(data={'day': result['dates'], 'cases': result['active']})
+  latest_day = result['dates'][-1]
+  cases_since_quarantine = np.array(data[data['day'] >= first_day_with_quarantine_effect]['cases'])
+  day_no_since_quarantine = np.array([d.toordinal() for d in data[data['day'] >= first_day_with_quarantine_effect]['day']])
+  params, _ = curve_fit(
+      corona_curve,
+      xdata=day_no_since_quarantine, 
+      ydata=cases_since_quarantine, 
+      p0=[
+        cases_since_quarantine[0],
+        first_day_with_quarantine_effect.toordinal(),
+        8e-9,
+        5.6e7],
+      bounds=(
+          [
+            0,
+            day_no_since_quarantine[0],
+            1e-11,
+            cases_since_quarantine[-1]
+          ],
+          [
+            cases_since_quarantine[-1],
+            dt.datetime(year=2021, month=6, day=1).toordinal(),
+            1e-8,
+            8.35e7
+          ]
+      )
+  )
+  data = data[data['day'] >= first_day_with_quarantine_effect]
+  projection_data = data.copy()
+  projection_data = projection_data.drop(['cases'], axis=1)
+  projection_data['curve'] = [corona_curve(d.toordinal(), *params) for d in projection_data['day']]
+  for i in range(180):
+      projection_data = projection_data.append(pd.DataFrame(
+          [[list(projection_data['day'])[-1] + dt.timedelta(days=1), 
+            corona_curve((list(projection_data['day'])[-1] + dt.timedelta(days=1)).toordinal(), *params)]],
+          columns=('day', 'curve')
+      ), ignore_index=True)
+  predicted = [int(round(d)) for d in projection_data['curve'][projection_data['day'] > latest_day].to_numpy()]
+  result['predicted'] = {
+    'from_date': (result['dates'][-1]+ dt.timedelta(days=1)).strftime('%Y-%m-%d'),
+    'active': predicted,
+  }
+  result['dates'] = [d.strftime('%Y-%m-%d') for d in result['dates']]
+  with open(json_file, 'w+') as out:
+    out.write(json.dumps(result, indent=2))
+
+if __name__ == '__main__':
+  main()
--- js/corona.js
+++ js/corona.js
@@ -1,10 +1,10 @@
+/* Copyright (c) 2020 Oliver Lau <oliver.lau@gmail.com> - All rights reserved. */
+
 (function (window) {
     'use strict'
+
     const Config = {
-        title: '<automatically set>',
-        confirmed: {
-            url: 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
-        },
+        title: null,
         active: {
             label: 'Active',
             type: 'bar',
@@ -18,14 +18,12 @@
             borderWidth: 0,
         },
         recovered: {
-            url: 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv',
             label: 'Recovered',
             type: 'bar',
             backgroundColor: '#7CDC58',
             borderWidth: 0,
         },
         deaths: {
-            url: 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
             label: 'Deaths',
             type: 'bar',
             backgroundColor: '#D16EDC',
@@ -45,88 +43,50 @@
             borderColor: 'rgb(88, 230, 36)',
             backgroundColor: '#444',
         },
-        extra: {
-            latest: {
-                url: 'COVID-19-web-data/data/cases_country.csv',
-            },
-        },
     }
-    const DataId = Object.entries(Config).filter(val => {
-        const [_, obj] = val
-        return obj.url
-    }).map(obj => obj[0])
     let el = {}
     let locale = 'de-DE'
     let ctx = null
-    let prediction_days = 0
-    let doubling_rate = Infinity
-    let curr_data = {}
     let chart = null
-    let selected_country = 'Germany'
-
-    const logistic = (x, b0, x0, k, s) => s * 1 / (1 + Math.exp(-1 * k * s * (x - x0)) * (s / b0 - 1))
-
-    const select = (tag, country) => {
-        const countries = curr_data[tag].rows.filter(value => value.country === country)
-        if (countries.length === 0) {
-            console.warn('no data available for country: ' + selected_country)
-            return {}
-        }
-        return countries[0]
-    }
-
-    const predict_exp = (confirmed, days) => {
-        const curr_cases = confirmed.active[confirmed.cases.length - 1]
-        const curr_date = confirmed.dates[confirmed.dates.length - 1]
-        confirmed.predicted = new Array(confirmed.cases.length).fill(null)
-        for (let d = 1; d <= days; ++d) {
-            confirmed.predicted.push(Math.round(curr_cases * Math.pow(2, d / doubling_rate)))
-            const date = new Date(curr_date)
-            date.setDate(curr_date.getDate() + d)
-            confirmed.dates.push(date)
-            confirmed.deaths.push(null)
-            confirmed.recovered.push(null)
-        }
-    }
-
-    const predict_logistic = (confirmed, days, start_date) => {
-
-    }
+    let prediction_days = 0
+    let confirmed = {}
 
     const update = data => {
-        if (data instanceof Array) {
-            for (const d of data.filter(v => v.which)) {
-                curr_data[d.which] = d
-            }
+        if (data) {
+            confirmed = Object.assign({}, data)
+            confirmed.dates = data.dates.map(date => new Date(date))
         }
-        const confirmed = select('confirmed', selected_country)
-        confirmed.dates = curr_data['confirmed'].dates.slice()
-        confirmed.deaths = select('deaths', selected_country).cases
-        confirmed.recovered = select('recovered', selected_country).cases
-        confirmed.active = new Array(confirmed.cases.length)
-        for (let i = 0; i < confirmed.cases.length; ++i) {
-            confirmed.active[i] = confirmed.cases[i] - confirmed.deaths[i] - confirmed.recovered[i]
+        const last_update = new Date(confirmed.latest.last_update)
+        document.getElementById('latest-date').innerText = `${last_update.toLocaleDateString(locale)} ${last_update.toLocaleTimeString(locale)}`
+        document.getElementById('latest-total').innerText = confirmed.latest.total.toLocaleString(locale)
+        document.getElementById('latest-active').innerText = confirmed.latest.active.toLocaleString(locale)
+        document.getElementById('latest-deaths').innerText = confirmed.latest.deaths.toLocaleString(locale)
+        document.getElementById('latest-recovered').innerText = confirmed.latest.recovered.toLocaleString(locale)
+        document.getElementById('App').classList.remove('hidden')
+        document.getElementById('loader-screen').classList.add('hidden')
+        const curr_date = confirmed.dates[confirmed.dates.length - 1]
+        let dates = [...confirmed.dates]
+        for (let day = 0; day < prediction_days; ++day) {
+            const date = new Date(curr_date)
+            date.setDate(curr_date.getDate() + day + 1)
+            dates.push(date)
         }
-        if (doubling_rate === Infinity) {
-            doubling_rate = Math.round(10 * confirmed.doubling_rates[confirmed.doubling_rates.length - 1]) / 10
-            el.doubling_rate.value = doubling_rate
-        }
-        predict_exp(confirmed, prediction_days)
-        confirmed.dates = confirmed.dates.map(d => d.toLocaleDateString(locale))
-        el.current_cases.innerText = confirmed.active[confirmed.active.length - 1]
-        el.current_date.innerText = confirmed.dates[confirmed.dates.length - 1 - prediction_days]
-        el.latest_cases.innerText = confirmed.predicted[confirmed.predicted.length - 1].toLocaleString(locale)
-        el.latest_date.innerText = confirmed.dates[confirmed.dates.length - 1]
+        const predicted = new Array(confirmed.active.length).fill(null).concat(confirmed.predicted.active.slice(0, prediction_days))
+        dates = dates.map(d => d.toLocaleDateString(locale))
+        el.current_date.innerText = dates[confirmed.dates.length - 1]
+        el.latest_date.innerText = dates[confirmed.dates.length - 1 + prediction_days]
+        el.current_cases.innerText = confirmed.active[confirmed.active.length - 1].toLocaleString(locale)
+        el.latest_cases.innerText = prediction_days > 0 ? confirmed.predicted.active[prediction_days - 1].toLocaleString(locale) : el.current_cases.innerText
         if (chart) {
-            chart.data.labels = confirmed.dates
-            chart.data.datasets[1].data = confirmed.predicted
+            chart.data.labels = dates
+            chart.data.datasets[3].data = predicted
             chart.update()
         }
         else {
             chart = new Chart(ctx, {
                 type: 'bar',
                 data: {
-                    labels: confirmed.dates,
+                    labels: dates,
                     datasets: [
                         {
                             data: confirmed.active,
@@ -162,7 +122,7 @@
                             order: 3,
                         },
                         {
-                            data: confirmed.predicted,
+                            data: predicted,
                             type: Config.predicted.type,
                             yAxisID: 'A',
                             label: Config.predicted.label,
@@ -226,123 +186,28 @@
         }
     }
 
-    const fetchOne = which => {
-        // console.debug(`Fetching '${which}' ...`)
-        return fetch(Config[which].url)
-            .then(response => {
-                return response.ok
-                    ? response.text()
-                    : Promise.reject(response.status)
-            })
-            .then(raw_data => {
-                const data = raw_data.split('\n')
-                const dates = data.shift().split(',').slice(4).map(v => new Date(v))
-                const rows = data.map(row => {
-                    const [province, country, lat, lon, ...cases] = row.split(',')
-                    const doubling_rates = [null]
-                    for (let i = 1; i < cases.length; ++i) {
-                        const prev_cases = cases[i - 1]
-                        const curr_cases = cases[i]
-                        if (prev_cases === curr_cases) {
-                            doubling_rates.push(null)
-                        }
-                        else if (prev_cases > 0 && curr_cases > 0) {
-                            const rate = curr_cases / prev_cases
-                            const doubling_rate = 1 / Math.log2(rate)
-                            doubling_rates.push(doubling_rate)
-                        }
-                        else {
-                            doubling_rates.push(null)
-                        }
-                    }
-                    return {
-                        province: province,
-                        country: country,
-                        where: {
-                            lat: parseFloat(lat),
-                            lon: parseFloat(lon)
-                        },
-                        cases: cases.map(v => +v),
-                        doubling_rates: doubling_rates,
-                    }
-                })
-                return {
-                    which: which,
-                    dates: dates,
-                    rows: rows,
-                }
-            })
-    }
-
-    const fetchLatest = () => {
-        // console.debug('Fetching latest web-data ...')
-        return fetch(Config.extra.latest.url)
-            .then(response => {
-                return response.ok
-                    ? response.text()
-                    : Promise.reject(response.status)
-            })
-            .then(raw_data => {
-                const data = raw_data.split('\n')
-                data.shift()
-                const selected_country_data = data
-                .map(row => {
-                    const [country, last_update, lat, lon, cases, deaths, recovered, active] = row.split(',')
-                    return {
-                        country: country,
-                        where: {
-                            lat: parseFloat(lat),
-                            lon: parseFloat(lon)
-                        },
-                        last_update: new Date(last_update),
-                        cases: +cases,
-                        deaths: +deaths,
-                        recovered: +recovered,
-                        active: +active,
-                    }
-                })
-                .filter(d => d.country === selected_country)
-                return (selected_country_data.length > 0)
-                ? selected_country_data[0]
-                : {}
-            })
-    }
-
-    const updateLatest = data => {
-        console.debug(data)
-        document.getElementById('latest-date').innerText = `${data.last_update.toLocaleDateString(locale)} ${data.last_update.toLocaleTimeString(locale)}`
-        document.getElementById('latest-total').innerText = data.cases
-        document.getElementById('latest-active').innerText = data.active
-        document.getElementById('latest-deaths').innerText = data.deaths
-        document.getElementById('latest-recovered').innerText = data.recovered
-    }
-
-    const fetchAll = () => {
-        fetchLatest().then(updateLatest)
-        const promises = DataId.map(which => fetchOne(which))
-        Promise.all(promises).then(data => {
-            document.getElementById('App').classList.remove('hidden')
-            document.getElementById('loader-screen').classList.add('hidden')
+    const main = () => {
+        fetch('data/current.json')
+        .then(response => {
+            return response.ok
+                ? response.json()
+                : Promise.reject(response.status)
+        })
+        .then(data => {
+            Config.title = `COVID-19 in ${data.country}`
+            el.prediction_days.setAttribute('max', data.predicted.active.length)
             update(data)
         })
-    }
-
-    const main = () => {
-        Config.title = `COVID-19 in ${selected_country}`
         ctx = document.getElementById('progress').getContext('2d')
         el.current_date = document.getElementById('current-date')
         el.current_cases = document.getElementById('current-cases')
         el.latest_date = document.getElementById('predicted-date')
         el.latest_cases = document.getElementById('predicted-cases')
         el.prediction_days = document.getElementById('prediction-days')
-        el.doubling_rate = document.getElementById('doubling-rate')
-        el.doubling_rate.addEventListener('change', evt => {
-            doubling_rate = +evt.target.value
-            update()
-        })
         prediction_days = +el.prediction_days.value
         el.prediction_days.addEventListener('change', evt => {
-            prediction_days = +evt.target.value
+            prediction_days = Math.min(+evt.target.value, +el.prediction_days.getAttribute('max'))
+            evt.target.value = prediction_days
             update()
         });
         [...document.getElementsByClassName('stepper')].forEach(stepper => {
@@ -350,7 +215,6 @@
             input.previousElementSibling.addEventListener('click', _ => { input.stepDown(); input.dispatchEvent(new Event('change')) })
             input.nextElementSibling.addEventListener('click', _ => { input.stepUp(); input.dispatchEvent(new Event('change')) })
         })
-        fetchAll()
     }
     window.addEventListener('load', main)
 })(window)

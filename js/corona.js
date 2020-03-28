aa1eff057e72854fb2ec74e4aa341004fb3508da
Oliver Lau <oliver@ersatzworld.net>
2020-03-28 15:07:40 +0100

    Added copyright statement.

--- js/corona.js
+++ js/corona.js
@@ -1,4 +1,20 @@
-/* Copyright (c) 2020 Oliver Lau <oliver.lau@gmail.com> - All rights reserved. */
+/*
+    COVID-19 spread visualization and active cases prediction
+    Copyright (c) 2020 Oliver Lau <oliver.lau@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 (function (window) {
     'use strict'

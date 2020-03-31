e0b7575739df6270b843c30a4ac1c7f3ec47bae6
Oliver Lau <oliver@ersatzworld.net>
2020-03-31 09:46:51 +0200

    Using minified CSS and JS files.

--- _bin/watch.sh
+++ _bin/watch.sh
@@ -1,3 +1,4 @@
 #!/usr/local/bin/bash
 
-watchify js/corona.js  -o 'uglify --js js/corona.js > static/corona.min.js' -v &
+watchify js/corona.js  -o 'minify --js js/corona.js > static/corona.min.js' -v &
+watchify js/corona.js  -o 'uglifycss css/default.css > static/corona.min.css' -v &

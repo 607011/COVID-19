83305a9622fbefab6b7dd163265e58a97f20c53a
Oliver Lau <oliver@ersatzworld.net>
2020-04-02 13:37:45 +0200

    Removed unnecessary watch.sh.

--- bin/watch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/bash
-
-watchify js/corona.js  -o 'minify --js js/corona.js > static/corona.min.js' -v &
-watchify js/corona.js  -o 'uglifycss css/default.css > static/corona.min.css' -v &

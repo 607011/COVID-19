dcf263852477d3eced1f4f34229bd27953179c0f
Oliver Lau <oliver@ersatzworld.net>
2020-04-04 10:41:41 +0200

    Removed unused code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {

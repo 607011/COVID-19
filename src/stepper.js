e5faf4565780e5abd8b7863d08b56f0a20b08e02
Oliver Lau <oliver@ersatzworld.net>
2020-04-20 10:08:05 +0200

    Made use of (dis)connectedCallback().

--- src/stepper.js
+++ src/stepper.js
@@ -12,10 +12,21 @@ export default class NumberStepper extends HTMLElement {
         cancelable: false,
         composed: true
       })
+      this.boundOnChange = function () {
+        this.dispatchEvent(this.changeEvent)
+      }.bind(this)
+      this.boundOnStepUp = function () {
+        this.input.stepUp()
+        this.dispatchEvent(this.changeEvent)
+      }.bind(this)
+      this.boundOnStepDown = function () {
+        this.input.stepDown()
+        this.dispatchEvent(this.changeEvent)
+      }.bind(this)
       const span = document.createElement('span')
       span.className = 'stepper'
       span.style.height = h
-      const downButton = document.createElement('button')
+      this.downButton = document.createElement('button')
       this.input = document.createElement('input')
       this.input.type = 'number'
       this.input.min = this.getAttribute('min') | 0
@@ -24,28 +35,17 @@ export default class NumberStepper extends HTMLElement {
       this.input.value = this.getAttribute('value') | 0
       this.input.style.width = this.getAttribute('innerwidth')
       this.input.style.height = h
-      this.input.addEventListener('change', function() {
-          this.dispatchEvent(this.changeEvent)
-      }.bind(this))
-      downButton.innerHTML = '&minus;'
-      downButton.title = `decrease by ${this.input.step}`
-      downButton.style.width = h
-      downButton.style.height = h
-      downButton.className = 'left'
-      downButton.addEventListener('click', function() {
-          this.input.stepDown()
-          this.dispatchEvent(this.changeEvent)
-      }.bind(this))
-      const upButton = document.createElement('button')
-      upButton.innerHTML = '&plus;'
-      upButton.title = `increase by ${this.input.step}`
-      upButton.style.width = h
-      upButton.style.height = h
-      upButton.className = 'right'
-      upButton.addEventListener('click', function() {
-          this.input.stepUp()
-          this.dispatchEvent(this.changeEvent)
-      }.bind(this))
+      this.downButton.innerHTML = '&minus;'
+      this.downButton.title = `decrease by ${this.input.step}`
+      this.downButton.style.width = h
+      this.downButton.style.height = h
+      this.downButton.className = 'left'
+      this.upButton = document.createElement('button')
+      this.upButton.innerHTML = '&plus;'
+      this.upButton.title = `increase by ${this.input.step}`
+      this.upButton.style.width = h
+      this.upButton.style.height = h
+      this.upButton.className = 'right'
       const style = document.createElement('style')
       style.textContent = `
 * {
@@ -98,29 +98,39 @@ button.right {
 button:active {
   background-color: #eee;
 }`
-      span.appendChild(downButton)
-      span.appendChild(this.input)
-      span.appendChild(upButton)
-      shadow.appendChild(style)
-      shadow.appendChild(span)
+    span.appendChild(this.downButton)
+    span.appendChild(this.input)
+    span.appendChild(this.upButton)
+    shadow.appendChild(style)
+    shadow.appendChild(span)
   }
   get value() {
-      return this.input.value
+    return this.input.value
   }
   set value(v) {
-      this.input.value = Math.max(+this.input.min, Math.min(v, +this.input.max))
+    this.input.value = Math.max(+this.input.min, Math.min(v, +this.input.max))
   }
   set max(v) {
-      this.input.max = v
-      this.input.value = Math.min(+this.input.value, v)
+    this.input.max = v
+    this.input.value = Math.min(+this.input.value, v)
   }
   get max() {
-      return this.input.max
+    return this.input.max
   }
   set step(v) {
     this.input.step = Math.max(v, 0)
   }
   get step() {
     return this.input.step
+  }
+  connectedCallback() {
+    this.input.addEventListener('change', this.boundOnChange)
+    this.upButton.addEventListener('click', this.boundOnStepUp)
+    this.downButton.addEventListener('click', this.boundOnStepDown)
+  }
+  disconnectedCallback() {
+    this.input.removeEventListener('change', this.boundOnChange)
+    this.upButton.removeEventListener('click', this.boundOnStepUp)
+    this.downButton.removeEventListener('click', this.boundOnStepDown)
   }
 }
